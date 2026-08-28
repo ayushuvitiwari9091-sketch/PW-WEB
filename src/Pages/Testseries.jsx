3517b13dc02c4e0e5dd758e325f906ec40d27c23
ayushuvitiwari9091-sketch <ayushuvitiwari9091@gmail.com>
2026-08-28 21:01:32 +0530

    updated project

--- src/Pages/Testseries.jsx
+++ src/Pages/Testseries.jsx
@@ -5,6 +5,7 @@ import Navbar from '../Components/Navbar'
 import Testimg from './Img/test-home.webp'
 import Tvicon from './Img/tv-image.webp'
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { FaArrowRight } from "react-icons/fa6";
 import Youtubecon from './Img/youtube.webp'
 import Cammera from './Img/cammera.webp'
 import Heading from '../Components/Heading'
@@ -12,6 +13,15 @@ import CourseCard from '../Components/CourseCard'
 import Neet from './Img/neet-course.webp'
 import GOVT from './Img/GOVT.webp'
 import AtoZ from './Img/atoz image.webp'
+import Erimg1 from './Img/erimg-1.webp'
+import Erimg2 from './Img/erimg2.webp'
+import Erimg3 from './Img/erimg3.webp'
+import Offlinecenter from './Img/offline-center.webp'
+import S1img from './Img/s1img.webp'
+import S2img from './Img/s2img.webp'
+import S3img from './Img/s3img.webp'
+import S4img from './Img/s4img.webp'
+import S5img from './Img/s5img.webp'
 
 const Testserise = () => {
 
@@ -50,6 +60,10 @@ const Testserise = () => {
             Heading: "Exam Categories",
             span: "PW is preparing students for 35+ exam categories. Scroll down to find the one you are preparing for",
         },
+        {
+            Heading: "Why PW Test Series?",
+            span: "Discover the key features of PW Test Series",
+        },
     ];
 
     const smallcardData = [
@@ -76,6 +90,57 @@ const Testserise = () => {
             span: "Acess our Test pass",
             icon: Cammera,
             bgColor: "#D1E9FF",
+        },
+    ];
+
+    const examsmallData = [
+        {
+            bgColor: "#F8F8FF",
+            Heading: "IIT JAM",
+            image: Erimg1,
+        },
+        {
+            bgColor: "#F8F8FF",
+            Heading: "ESE GATE",
+            image: Erimg2,
+        },
+        {
+            bgColor: "#F8F8FF",
+            Heading: "AE/JE",
+            image: Erimg3,
+        },
+    ];
+
+    const TestData = [
+        {
+            image: Offlinecenter,
+            span: "Designed by Top Faculties",
+            bgColor: "#FFF9E8",
+        },
+        {
+            image: S1img,
+            span: "Latest Pattern",
+            bgColor: "#F3F5FF",
+        },
+        {
+            image: S2img,
+            span: "Elaborate Results",
+            bgColor: "#FFF1EF",
+        },
+        {
+            image: S3img,
+            span: "State & All India Rank",
+            bgColor: "#FFF6EE",
+        },
+        {
+            image: S4img,
+            span: "Cross Platform",
+            bgColor: "#F4FBFF",
+        },
+        {
+            image: S5img,
+            span: "Available for all categories",
+            bgColor: "#ECEFFF",
         },
     ];
     return (
@@ -131,10 +196,38 @@ const Testserise = () => {
                 }
             </div>
 
-            <div>
-                <div>
-                    
-                </div>
+            <div className='h-auto w-[80%] mx-auto mt-5 grid grid-cols-3'>
+                {
+                    examsmallData.map((index) => (
+                        <div className='h-36 w-91 rounded-[6px] flex group shadow-[0_0px_20px_rgba(0,0,0,0.08)] bg-[#FFFFFF] overflow-hidden relative border border-transparent hover:border-black transition-all duration-300'>
+                            <div>
+                                <span className='inline-block mt-6 ml-5 text-2xl font-semibold'>{index?.Heading}</span>
+                                <div className='flex items-center justify-start group h-10 w-[100%] mt-4'>
+                                    <span className='p-4  group-hover:text-blue-600 underline'>Explore Category</span>
+                                    <button className='bg-[#F8F8F8] pt-2 pb-2 pl-4 pr-4 rounded-2xl  group-hover:bg-blue-600 group-hover:text-white'><FaArrowRight /></button>
+                                </div>
+                            </div>
+                            <div className='h-40 w-40 rounded-[50%] absolute top-[-10px] right-[-60px] flex items-center pl-5' style={{ backgroundColor: index.bgColor }}>
+                                <img src={index?.image} alt="" className='h-15 transition-transform duration-200 group-hover:scale-110' />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className='mt-15'>
+                <Heading Headingtext={HeadingData[1]} />
+            </div>
+
+            <div className='h-auto w-[78%] grid grid-cols-3 mx-auto mt-15  gap-5'>
+                {
+                    TestData.map((item) => (
+                        <div className='h-35 w-92 rounded-[10px] flex flex-col items-center justify-center gap-4' style={{backgroundColor: item?.bgColor}}>
+                            <img src={item?.image} alt="" className='h-15' />
+                            <span className='font-semibold text-[20px]'>{item?.span}</span>
+                        </div>
+                    ))
+                }
             </div>
 
 
